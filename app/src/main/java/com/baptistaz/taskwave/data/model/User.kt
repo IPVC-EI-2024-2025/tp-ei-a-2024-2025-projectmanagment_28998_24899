f1bd9fbb1e0efd60bca738934a1e954af56f7bf6
BaptistaZ <tiagobaptista@ipvc.pt>
2025-05-27 15:54:09 +0100

    Add base folder structure and Add User model

---
+++ app/src/main/java/com/baptistaz/taskwave/data/model/User.kt
@@ -0,0 +1,12 @@
+package com.baptistaz.taskwave.data.model
+
+data class User(
+    val id_User: String = "",
+    val name: String = "",
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val profileType: String = "",
+    val photo: String = "",
+    val phoneNumber: String = ""
+)
